142b0c538461dcc3eec37ddf3845cbd3ae9ba608
Patrick Fung <patrick@makestuffdostuff.com>
2016-03-07 17:29:02 -0500

    add configuration for deploying updates to AtoM server

--- knife.rb
+++ knife.rb
@@ -1,8 +1,10 @@
 local_mode true
-chef_repo_path   File.expand_path('../' , __FILE__)
+chef_repo_path    File.expand_path('../' , __FILE__)
+cookbook_path   [ File.expand_path('../berks-cookbooks' , __FILE__) ]
 
 knife[:ssh_attribute] = "knife_zero.host"
 knife[:use_sudo] = true
+knife[:host_key_verify] = false
 
 ## use specific key file to connect server instead of ssh_agent(use ssh_agent is set true by default).
 # knife[:identity_file] = "~/.ssh/id_rsa"
@@ -25,3 +27,9 @@ knife[:automatic_attribute_whitelist] = %w[
   cloud_v2
   chef_packages
 ]
+
+## knife.rb will be read twice at run. (before launch chef-zero locally and before run knife.)
+## And configuration for chef-zero doesn't has key `color`.
+if ARGV[0..1] == ["zero", "converge"] && ! Chef::Config.has_key?(:color)
+  system('berks vendor')
+end
