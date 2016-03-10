2b30bf8b0c1e3ad76c7220db428bc6b8d7201447
Patrick Fung <patrick@makestuffdostuff.com>
2016-03-10 15:48:34 -0500

    rubocops

--- knife.rb
+++ knife.rb
@@ -1,8 +1,8 @@
 local_mode true
-chef_repo_path    File.expand_path('../' , __FILE__)
-cookbook_path   [ File.expand_path('../berks-cookbooks' , __FILE__) ]
+chef_repo_path File.expand_path('../', __FILE__)
+cookbook_path [File.expand_path('../berks-cookbooks', __FILE__)]
 
-knife[:ssh_attribute] = "knife_zero.host"
+knife[:ssh_attribute] = 'knife_zero.host'
 knife[:use_sudo] = true
 knife[:host_key_verify] = false
 
@@ -11,7 +11,7 @@ knife[:host_key_verify] = false
 
 ## Attributes of node objects will be saved to json file.
 ## the automatic_attribute_whitelist option limits the attributes to be saved.
-knife[:automatic_attribute_whitelist] = %w[
+knife[:automatic_attribute_whitelist] = %w(
   fqdn
   os
   os_version
@@ -26,10 +26,10 @@ knife[:automatic_attribute_whitelist] = %w[
   cloud
   cloud_v2
   chef_packages
-]
+)
 
 ## knife.rb will be read twice at run. (before launch chef-zero locally and before run knife.)
 ## And configuration for chef-zero doesn't has key `color`.
-if ARGV[0..1] == ["zero", "converge"] && ! Chef::Config.has_key?(:color)
+if ARGV[0..1] == %w('zero', 'converge') && !Chef::Config.key?(:color)
   system('berks vendor')
 end
